2a2fcf828a45d401a81e1c486eeb8da77d0cb2ce
Iggy <igoririanto@rocketmail.com>
2016-10-13 16:57:45 -0700

    event

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,7 @@
-var animatePoints = function(){
+var pointsArray = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');
+var animatePoints = function(points){
+
   var revealAllPoints = function(){
     for(var i = 0; i < points.length; i++){
       points[i].style.opacity = 1;
@@ -11,3 +12,17 @@ var animatePoints = function(){
   };
   revealAllPoints();
 };
+
+window.onload = function(){
+
+  if (window.innerHeight > 950){ //landing page is about 950px
+    animatePoints(pointsArray);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener('scroll', function(event){
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
