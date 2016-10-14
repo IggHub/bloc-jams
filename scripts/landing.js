29a418a47fc0a0b62bf1b298721f0e99b401679d
Iggy <igoririanto@rocketmail.com>
2016-10-13 17:31:13 -0700

    add forEach external iterator callback

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points){
+var revealAllPoints = function(point){
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform="scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
-  var revealAllPoints = function(){
-    for(var i = 0; i < points.length; i++){
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform="scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-  };
-  revealAllPoints();
+var animatePoints = function(points){
+  forEach(points, revealAllPoints);
 };
 
 window.onload = function(){
