a40af62a7601fd0fefb2744c5152bf22505db75f
Iggy <igoririanto@rocketmail.com>
2016-10-19 18:34:24 -0400

    refactor landing with jquery

--- scripts/landing.js
+++ scripts/landing.js
@@ -7,20 +7,27 @@ var revealAllPoints = function(point){
     point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
 
-var animatePoints = function(points){
-  forEach(points, revealAllPoints);
+var animatePoints = function(){
+  var revealPoint = function() {
+    $(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
+    });
+  };
+  $.each($('.point'), revealPoint);
 };
 
-window.onload = function(){
-
-  if (window.innerHeight > 950){ //landing page is about 950px
-    animatePoints(pointsArray);
+$(window).load(function(event){ // originally window.onload = function(); window is the object (entire); $(window). onload = function() becomes .load(function)
+  if ($(window).height() > 950){
+    animatePoints();
   }
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-  window.addEventListener('scroll', function(event){
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);
+
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+    $(window).scroll(function(event){
+      if ($(window).scrollTop() >= scrollDistance){
+      animatePoints();
     }
   });
-}
+
+});
