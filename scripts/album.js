96ecb1348884d8a9b4f0b5bdf2bcf39a240cb985
Iggy <igoririanto@rocketmail.com>
2016-10-20 16:06:20 -0400

    refactor album.js

--- scripts/album.js
+++ scripts/album.js
@@ -1,18 +1,19 @@
 //lists album1's attributes
 var albumPicasso = {
-    title: 'The Colors',
+    name: 'The Colors',
     artist: 'Pablo Picasso',
     label: 'Cubism',
     year: '1881',
     albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21'},
-        { title: 'Magenta', duration: '2:15'}
+        { name: 'Blue', length: '4:26' },
+        { name: 'Green', length: '3:14' },
+        { name: 'Red', length: '5:01' },
+        { name: 'Pink', length: '3:21' },
+        { name: 'Magenta', length: '2:15' }
     ]
 };
+
 
 //lists album2's attributes
 var albumMarconi = {
@@ -44,133 +45,81 @@ var albumPastry = {
     {title: 'Plain Donut', duration: '2:30'}
   ]
 };
-
-//create template for each SongRow
 var createSongRow = function(songNumber, songName, songLength) {
-     var template =
-        '<tr class="album-view-song-item">' //each line
-      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>' //anything starting with 'data-' is a data attribute; where is it being stored?
+    var template =
+        '<tr class="album-view-song-item">'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
       ;
 
-     return $(template);
- };
+    var $row = $(template);
 
-//get corresponding album DOM
-var $albumTitle = $('.album-view-title'); //get .album-view-title (class) element.
-var $albumArtist = $('.album-view-artist');
-var $albumReleaseInfo = $('.album-view-release-info');
-var $albumImage = $('.album-cover-art');
-var $albumSongList = $('.album-view-song-list');
+    var clickHandler = function() {
+        var songNumber = $(this).attr('data-song-number');
 
-//puts together album into HTML
+        if (currentlyPlayingSong !== null) {
+            var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
+            currentlyPlayingCell.html(currentlyPlayingSong);
+        }
+        if (currentlyPlayingSong !== songNumber) {
+            $(this).html(pauseButtonTemplate);
+            currentlyPlayingSong = songNumber;
+        } else if (currentlyPlayingSong === songNumber) {
+            $(this).html(playButtonTemplate);
+            currentlyPlayingSong = null;
+        }
+    };
+
+    var onHover = function(event) {
+        var songNumberCell = $(this).find('.song-item-number');
+        var songNumber = songNumberCell.attr('data-song-number');
+
+        if (songNumber !== currentlyPlayingSong) {
+            songNumberCell.html(playButtonTemplate);
+        }
+    };
+
+    var offHover = function(event) {
+        var songNumberCell = $(this).find('.song-item-number');
+        var songNumber = songNumberCell.attr('data-song-number');
+
+        if (songNumber !== currentlyPlayingSong) {
+            songNumberCell.html(songNumber);
+        }
+    };
+
+    $row.find('.song-item-number').click(clickHandler);
+    $row.hover(onHover, offHover);
+    return $row;
+};
 
 var setCurrentAlbum = function(album) {
-  $albumTitle.text(album.title); //jQuery's .text method replaces content of text nodes
-  $albumArtist.text(album.artist);//execute .text() method! Does text() action.
-  $albumReleaseInfo.text(album.year + ' ' + album.label); //.text() replaces content while .append() adds content!
-  $albumImage.attr('src', album.albumArtUrl);
+    var $albumTitle = $('.album-view-title');
+    var $albumArtist = $('.album-view-artist');
+    var $albumReleaseInfo = $('.album-view-release-info');
+    var $albumImage = $('.album-cover-art');
+    var $albumSongList = $('.album-view-song-list');
 
-  $albumSongList.empty(); //run the jQuery function empty(); to empty it. equal to .innerHTML = '';
+    $albumTitle.text(album.name);
+    $albumArtist.text(album.artist);
+    $albumReleaseInfo.text(album.year + ' ' + album.label);
+    $albumImage.attr('src', album.albumArtUrl);
 
+    $albumSongList.empty();
 
-   for (var i = 0; i < album.songs.length; i++) {
-      var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-      $albumSongList.append($newRow);
-   } // i+1 because song starts with song #1.
-};
-
-var findParentByClassName = function(element, targetClass){
-  var currentParent = element.parentElement;
-  if (element){
-   //returns element's parent
-    while(currentParent.className != targetClass && currentParent.className !== null){ //currentParent(current element's parent does not equal to targetClass (its parent is not itself), and it HAS at least a parent element (not null)) This loop goes up until hitting the right target parent! BRILLIANT!
-        currentParent = currentParent.parentELement; // currentParent (element's parent) is now element's parent's parent
-
-    }
-    return currentParent;
-  }
-
-  if (findParentByClassName(element, targetClass) == undefined){
-    alert("No parent found");
-  } else if (!currentParent) {
-    alert("No parent found with that class name");
-  }
-};
-//finds Parent, given Class Name
-
-
-
-//getSongItem() method
-
-var getSongItem = function(element){ //how does it now song 1 and not song 2, or 3?
-  switch(element.className){
-    case 'song-item-title':
-    case 'song-item-duration':
-      return findParentByClassName(element/*element is either 'song-item-title' or song-item-duration'*/, 'album-view-song-item'/*this is their parent*/).querySelector('.song-item-number'); //what does querySelector does, exactly? // need to go higher one class (a parent higher), then go down using querySelector
-    case 'song-item-number':
-      return element;
-    case 'album-song-button':
-    case 'ion-pause':
-    case 'ion-play':
-      return findParentByClassName(element, 'song-item-number');//parent; why is it song-item-number?
-    case 'album-view-song-item':
-      return element.querySelector('.song-item-number'); //what kind of object does querySelector return?
-    default:
-      return;
-  }
-};
-
-
-var clickHandler = function(targetElement) {
-    var songItem = getSongItem(targetElement);
-
-    if (currentlyPlayingSong === null) {
-        songItem.innerHTML = pauseButtonTemplate;
-        currentlyPlayingSong = songItem.getAttribute('data-song-number');
-    } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-        songItem.innerHTML = playButtonTemplate;
-        currentlyPlayingSong = null;
-    } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-        var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-        currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-        songItem.innerHTML = pauseButtonTemplate;
-        currentlyPlayingSong = songItem.getAttribute('data-song-number');
+    for (i = 0; i < album.songs.length; i++) {
+        var $newRow= createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+        $albumSongList.append($newRow);
     }
 };
 
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item'); //gets array of album-view-song-items
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
 var currentlyPlayingSong = null;
 
-window.onload = function() {
+$(document).ready(function() {
     setCurrentAlbum(albumPicasso);
-
-    songListContainer.addEventListener('mouseover', function(event) {
-        if (event.target.parentElement.className === 'album-view-song-item') {
-            var songItem = getSongItem(event.target);
-
-            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
-                songItem.innerHTML = playButtonTemplate;
-            }
-        }
-    });
-
-    for (i = 0; i < songRows.length; i++) {
-        songRows[i].addEventListener('mouseleave', function(event) {
-            var songItem = getSongItem(event.target);
-            var songItemNumber = songItem.getAttribute('data-song-number');
-
-            if (songItemNumber !== currentlyPlayingSong) {
-                songItem.innerHTML = songItemNumber;
-            }
-        });
-        songRows[i].addEventListener('click', function(event) {
-             clickHandler(event.target);
-        });
-    }
-};
+});
